b573dae1b475d6bec4980df665dd4598fec72e4e
PHGURU <45846591+PHGURU@users.noreply.github.com>
2019-09-18 14:18:08 +0530

    Frist Commit

---
+++ resources/js/components/UsersComponent.vue
@@ -0,0 +1,84 @@
+<template>
+        <div class="row mt-5">
+          <div class="col-md-12">
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">Users List</h3>
+
+                <div class="card-tools">
+
+
+                <button class="btn btn-success"  data-toggle="modal" data-target="#modal-default"><i class="fas fa-user-plus"></i> Add New</button>
+
+                </div>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body table-responsive p-0">
+                <table class="table table-hover">
+                  <thead>
+                    <tr>
+                      <th>ID</th>
+                      <th>Name</th>
+                      <th>Email</th>
+                      <th>Type</th>
+                      <th>Action</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>183</td>
+                      <td>John Doe</td>
+                      <td>11-7-2014</td>
+                      <td><span class="tag tag-success">Approved</span></td>
+                      <td>
+                        <a class="btn btn-warning"><i class="fas fa-pencil-alt"></i></a>
+                         <a class="btn btn-danger"><i class="fas fa-trash"></i></a>
+                      </td>
+                    </tr>
+
+                  </tbody>
+                </table>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </div>
+
+              <!-- Modal -->
+
+         <div class="modal fade show" id="modal-default">
+                    <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                        <h4 class="modal-title">Add New User</h4>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">×</span>
+                        </button>
+                        </div>
+                        <div class="modal-body">
+                        <p>One fine body…</p>
+                        </div>
+                        <div class="modal-footer">
+                        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Create</button>
+                        </div>
+                    </div>
+                    <!-- /.modal-content -->
+                    </div>
+                    <!-- /.modal-dialog -->
+        </div>
+
+
+        <!-- Modal -->
+        </div>
+
+
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
